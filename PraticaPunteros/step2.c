41b13650f240a9dc2b7e3ee5d13f2c177936b5cf
Yosery Zheng <liqing.zheng@ucr.ac.cr>
2024-04-02 13:33:11 +0000

    copiar step1b a step2

---
+++ PraticaPunteros/step2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct student {
+    char name[10];
+    int age;
+    int score;
+};
+
+void print_student(const struct student* s) {
+    printf("name: %s, age: %d, score: %d\n", s->name, s->age, s->score);
+    ((struct student*)s)->age = 5;
+}
+
+void main() {
+    struct student* s1 = malloc(sizeof(struct student));
+    s1->age = 20;
+    s1->score = 100;
+    strcpy(s1->name, "Maria");
+
+    printf("Tamaño de s1: %zu\n", sizeof(s1));
+    print_student(s1);
+    printf("Tamaño de *s1: %zu\n", sizeof(*s1));
+    print_student(s1);
+
+}
